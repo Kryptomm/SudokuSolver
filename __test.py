72cf33eec7b8db3a07d725e7d6132237c3d0e966
Tom Keller <tom11032002@gmail.com>
2024-05-27 17:07:49 +0200

    rework

--- __test.py
+++ __test.py
@@ -1,6 +1,24 @@
-import numpy as np
-from collections import Counter
+# Using colorama
+from colorama import Fore, Back, Style, init
+from termcolor import colored
 
-A = set([1])
+# Initialize colorama
+init()
 
-print(A)
+# Colorama examples
+print(Fore.RED + 'This is red text (colorama)')
+print(Fore.GREEN + 'This is green text (colorama)')
+print(Back.YELLOW + 'This has a yellow background (colorama)')
+print(Style.DIM + 'This is dim text (colorama)')
+print(Style.RESET_ALL + 'Back to normal text (colorama)')
+
+# Termcolor examples
+print(colored('This is red text (termcolor)', 'red'))
+print(colored('This is green text (termcolor)', 'green'))
+print(colored('This text is on a yellow background (termcolor)', 'white', 'on_yellow'))
+print(colored('This is blue text with underlined style (termcolor)', 'blue', attrs=['underline']))
+
+# Custom combinations
+print(colored('This text is bold and red with a yellow background (termcolor)', 'red', 'on_yellow', ['bold']))
+print(colored('This text is blue and underlined (termcolor)', 'blue', attrs=['underline']))
+print(colored('This text is green with reversed colors (termcolor)', 'green', attrs=['reverse']))
--- solver.py
+++ solver.py
@@ -1,159 +1,38 @@
-import numpy as np
 import sudokuRequester
 
-from sudoku import Sudoku
+import numpy as np
+import itertools
+
 from collections import Counter
+from colorama import Fore, Back, Style, init
+from termcolor import colored
 
-class Solver:
-    def __init__(self, sudoku : Sudoku) -> None:
-        self.sudoku = sudoku
-        self.possibleNumbers = []
+#Constants
+INDICES = list(range(8))
 
-        self.__reset_possible_numbers()
-        print("Sudoku loaded")
+#init colorama
+init()
 
-    def __reset_possible_numbers(self) -> None:
-        self.possibleNumbers = np.array([[set([i+1 for i in range(self.sudoku.box_size[0] * self.sudoku.box_size[1])]) if self.sudoku.grid[y][x] == 0 else set() for x in range(self.sudoku.cols)] for y in range(sudoku.rows)])
-
-    def __sort_out_rows(self) -> None:
-        for row in range(self.sudoku.rows):
-            nums = set(self.sudoku.grid[row])
-            for col in range(self.sudoku.cols):
-                self.possibleNumbers[row][col] -= nums
-
-    def __sort_out_columns(self) -> None:
-        for col in range(self.sudoku.cols):
-            nums = set(self.sudoku.grid.T[col])
-            for row in range(self.sudoku.rows):
-                self.possibleNumbers[row][col] -= nums
-
-    def __sort_out_boxes(self) -> None:
-        for box_row in range(self.sudoku.box_rows):
-            for box_col in range(self.sudoku.box_cols):
-                box_top_left =      (box_row * self.sudoku.box_size[1], box_col * self.sudoku.box_size[0])
-                box_bottom_right =  ((box_row + 1) * self.sudoku.box_size[1], (box_col + 1) * self.sudoku.box_size[0])
-                
-                box = self.sudoku.grid[range(box_top_left[0], box_bottom_right[0]),:]
-                box = box[:,range(box_top_left[1], box_bottom_right[1])]
-
-                box = set(box.flatten())
-                for row in range(box_top_left[0], box_bottom_right[0]):
-                    for col in range(box_top_left[1], box_bottom_right[1]):
-                        self.possibleNumbers[row][col] -= box
-
-    def __sort_out_possible_numbers(self) -> None:
-        self.__sort_out_rows()
-        self.__sort_out_columns()
-        self.__sort_out_boxes()
-
-    def __place_lonely_numbers(self) -> int:
-        placed = 0
-        for row in range(self.sudoku.rows):
-            for col in range(self.sudoku.cols):
-                if len(self.possibleNumbers[row][col]) != 1: continue
-                
-                elem = self.possibleNumbers[row][col].pop()
-                self.sudoku.grid[row][col] = elem
-                self.possibleNumbers[row][col].add(elem)
-                placed += 1
-                
-        return placed
-
-    def __place_lonely_row_numbers(self) -> int:
-        placed = 0
-        for row in range(self.sudoku.rows):
-            flattened_list = [num for subset in self.possibleNumbers[row] for num in subset]
-            occurences = dict(Counter(flattened_list))
-            
-            for col in range(self.sudoku.cols):
-                for pn in self.possibleNumbers[row][col]:
-                    if occurences[pn] == 1:
-                        self.sudoku.grid[row][col] = pn
-                        placed += 1
-                        break
-            
-        return placed
-    
-    def __place_lonely_col_numbers(self) -> int:
-        placed = 0
+def print_sudoku(new_state, state_before) -> None:
+    output = ""
+    for row, col in itertools.product(range(9), range(9)):
+        if new_state[row][col] == state_before[row][col]:
+            output += " " + str(new_state[row][col])
+        else:
+            output += " " + Fore.GREEN + str(new_state[row][col])
         
-        for col in range(self.sudoku.cols):
-            flattened_list = [num for subset in self.possibleNumbers.T[col] for num in subset]
-            occurences = dict(Counter(flattened_list))
-            
-            for row in range(self.sudoku.rows):
-                for pn in self.possibleNumbers[row][col]:
-                    if occurences[pn] == 1:
-                        self.sudoku.grid[row][col] = pn
-                        placed += 1
-                        break
-        
-        return placed
+        if col != 8 and col % 3 == 2: output += " |"
+        if col == 8 and row != 8: output += "\n"
+        if row % 3 == 2 and col == 8 and row != 8: output += " - - - + - - - + - - -\n"
     
-    def __place_lonley_box_numbers(self) -> int:
-        placed = 0
-        for box_row in range(self.sudoku.box_rows):
-            for box_col in range(self.sudoku.box_cols):
-                box_top_left =      (box_row * self.sudoku.box_size[1], box_col * self.sudoku.box_size[0])
-                box_bottom_right =  ((box_row + 1) * self.sudoku.box_size[1], (box_col + 1) * self.sudoku.box_size[0])
-                
-                box = self.possibleNumbers[range(box_top_left[0], box_bottom_right[0]),:]
-                box = box[:,range(box_top_left[1], box_bottom_right[1])]
-                box = box.flatten()
-                
-                flattened_list = [num for subset in box for num in subset]
-                occurences = dict(Counter(flattened_list))
-                
-                for row in range(box_top_left[0], box_bottom_right[0]):
-                    for col in range(box_top_left[1], box_bottom_right[1]):
-                        for pn in self.possibleNumbers[row][col]:
-                            if occurences[pn] == 1:
-                                self.sudoku.grid[row][col] = pn
-                                placed += 1
-                                break
+    print(output)
 
-        return placed
-    
-    def __place_possible_numbers(self) -> int:
-        placed = 0
-
-        placed += self.__place_lonely_numbers()
-        placed += self.__place_lonely_row_numbers()
-        placed += self.__place_lonely_col_numbers()
-        placed += self.__place_lonley_box_numbers()
-
-        return placed
-
-    def is_solved(self) -> bool:
-        return np.all(self.sudoku.grid)
-
-    def solve(self) -> bool:
-        iter = 1
-        print(sudoku)
-        while not self.is_solved():
-            self.__reset_possible_numbers()
-            print(f"Iteration: {iter}")
-            self.__sort_out_possible_numbers()
-
-            placed = self.__place_possible_numbers()
-            print(f"Placed {placed} tiles!\n")
-            if placed == 0:
-                print(sudoku)
-                for i in self.possibleNumbers:
-                    print(i)
-                return False
-
-            print(self.sudoku)
-            #Security for no endless loop
-            if iter > (self.sudoku.rows * self.sudoku.cols):
-                return False
-            iter += 1
-        return True
+def solve(grid : list) -> np.ndarray:
+    grid = np.array(grid)
+    copied_grid = grid.copy()
+    copied_grid[8][8] = 2
+    print_sudoku(grid, copied_grid)
 
 if __name__ == "__main__":
     grid = sudokuRequester.get_random_sudoko_difficulty("easy")
-    
-    sudoku = Sudoku(grid)
-    solver = Solver(sudoku)
-    print(solver.solve())
-    
+    solve(grid)
